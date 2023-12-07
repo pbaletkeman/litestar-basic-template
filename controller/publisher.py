e5f2ce5f981072326796f2ba461f696476af381e
Pete Letkeman <pete.letkeman@rakuten.com>
2023-12-07 15:14:46 -0500

    completed publisher create with and without books

--- controller/publisher.py
+++ controller/publisher.py
@@ -14,6 +14,8 @@ from litestar.params import Parameter
 from litestar.repository.filters import LimitOffset, OrderBy
 from pydantic import TypeAdapter
 
+from controller.book import provide_book_repo, BookRepository
+from model.book import NewBook, Book
 from model.publisher import Publisher, PublisherDTO, PublisherCreate
 
 if TYPE_CHECKING:
@@ -40,6 +42,7 @@ class PublisherController(Controller):
     path = '/publisher'
     dependencies = {
         'publisher_repo': Provide(provide_publisher_repo),
+        'book_repo': Provide(provide_book_repo)
     }
     publisher_controller_tag = ['Publisher - CRUD']
 
@@ -78,19 +81,42 @@ class PublisherController(Controller):
             raise HTTPException(detail=str(ex), status_code=status_codes.HTTP_404_NOT_FOUND)
 
     @post(tags=publisher_controller_tag)
-    async def create_publisher(self, publisher_repo: PublisherRepository,
+    async def create_publisher(self,
+                               publisher_repo: PublisherRepository,
+                               book_repo: BookRepository,
                                data: PublisherCreate, ) -> PublisherDTO:
         """Create a new ."""
+        """
+        {
+          "name": "pubName",
+          "sort_order": 5,
+          "books": [
+              {"name": "bookName1", "sort_order": 70},
+              {"name": "bookName2", "sort_order": 50},
+              {"name": "bookName3", "sort_order": 100},
+              {"name": "bookName4", "sort_order": 10}
+          ]
+        }
+        """
         try:
             _data = data.model_dump(exclude_unset=True, by_alias=False, exclude_none=True)
-            # books = _data.pop('books')
+            books: list[NewBook] | None = None
+            if 'books' in _data:
+                books = _data.pop('books')
 
             pub = Publisher(**_data)
-
             publisher_repo.session.add(pub)
             await publisher_repo.session.commit()
+            return_entity = pub.__json__()
 
-            return PublisherDTO.model_validate(pub.__json__())
+            if books:
+                for b in books:
+                    b['publisher_id'] = pub.id
+                book_items = await book_repo.add_many([Book(**b) for b in books])
+                await book_repo.session.commit()
+                return_entity['books'] = [b.__json__() for b in book_items]
+
+            return PublisherDTO.model_validate(return_entity)
 
         except Exception as ex:
             raise HTTPException(detail=str(ex), status_code=status_codes.HTTP_404_NOT_FOUND)
